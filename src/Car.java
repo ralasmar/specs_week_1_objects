d5377411cac48b12ad02b4823cc1fe7a53ccb90c
Rola Alasmar <alasm002@umn.edu>
2023-05-31 16:18:11 -0500

    objects lesson

---
+++ src/Car.java
@@ -0,0 +1,16 @@
+//car class that serves as a blueprint for car objects
+public class Car {
+    String make;
+    double price;
+    int year;
+    String color;
+
+    public Car(String make, double price, int year, String color){
+    //this refers to current constructor
+        this.make = make;
+        this .price = price;
+        this.year = year;
+        this.color = color;
+    }
+}
+
